56b18dba92eb4ef35805647d8ce4b0ff87db097e
Naman Pandey <2492004namanpandey@gmail.com>
2025-03-03 20:26:49 +0530

    fix: admin login

--- backend/models/Admin.js
+++ backend/models/Admin.js
@@ -9,11 +9,11 @@ const adminSchema = new mongoose.Schema({
 }, { timestamps: true });
 
 // Encrypt password before saving
-adminSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
-  const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt);
-});
+// adminSchema.pre("save", async function (next) {
+//   if (!this.isModified("password")) return next();
+//   const salt = await bcrypt.genSalt(10);
+//   this.password = await bcrypt.hash(this.password, salt);
+// });
 
 const Admin = mongoose.model("Admin", adminSchema);
 
